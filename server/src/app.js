ea33cae03cae62240274ed81592201dfac2a97cf
SAIF CHAUDHARY <SAIFCHOUDHARY.0666@GMAIL.COM>
2026-08-06 12:51:28 +0530

    Backend Setup

---
+++ server/src/app.js
@@ -0,0 +1,27 @@
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+
+const app = express();
+
+app.use(
+    cors({
+        origin: "http://localhost:5173",
+        credentials: true,
+    })
+);
+
+app.use(express.json());
+
+app.use(express.urlencoded({ extended: true }));
+
+app.use(cookieParser());
+
+app.get("/", (req, res) => {
+    res.json({
+        success: true,
+        message: "Fast Food POS API Running",
+    });
+});
+
+export default app;
