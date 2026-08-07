198f64014775a9a8bc05b01e4a22dbcdca01f43e
SAIF CHAUDHARY <SAIFCHOUDHARY.0666@GMAIL.COM>
2026-08-07 16:48:04 +0530

    Fix CORS issue

--- server/src/controllers/auth.controller.js
+++ server/src/controllers/auth.controller.js
@@ -1,56 +1,61 @@
 import asyncHandler from "../utils/asyncHandler.js";
 import ApiResponse from "../utils/ApiResponse.js";
-import { loginService, registerService,getMeService, forgotPasswordService } from "../services/auth.service.js";
+import {
+  loginService,
+  registerService,
+  getMeService,
+  forgotPasswordService,
+} from "../services/auth.service.js";
 
 export const register = asyncHandler(async (req, res) => {
-    const user = await registerService(req.body);
+  const user = await registerService(req.body);
 
-    return res
-        .status(201)
-        .json(new ApiResponse(201, user, "Admin registered successfully"));
+  return res
+    .status(201)
+    .json(new ApiResponse(201, user, "Admin registered successfully"));
 });
 
 export const login = asyncHandler(async (req, res) => {
-    const { token, user } = await loginService(req.body);
+  const { token, user } = await loginService(req.body);
 
-    const options = {
-        httpOnly: true,
-        secure: process.env.NODE_ENV === "production",
-        sameSite: "strict",
-        maxAge: 24 * 60 * 60 * 1000,
-    };
+  const options = {
+    httpOnly: true,
+    secure: true,
+    sameSite: "none",
+    maxAge: 24 * 60 * 60 * 1000,
+  };
 
-    return res
-        .status(200)
-        .cookie("accessToken", token, options)
-        .json(new ApiResponse(200, { user }, "Login successful"));
+  return res
+    .status(200)
+    .cookie("accessToken", token, options)
+    .json(new ApiResponse(200, { user }, "Login successful"));
 });
 
 export const logout = asyncHandler(async (req, res) => {
-    await logoutService();
+  await logoutService();
 
-    return res
-        .clearCookie("accessToken", {
-            httpOnly: true,
-            secure: process.env.NODE_ENV === "production",
-            sameSite: "strict",
-        })
-        .status(200)
-        .json(new ApiResponse(200, null, "Logout successful"));
+  return res
+    .clearCookie("accessToken", {
+      httpOnly: true,
+      secure: true,
+      sameSite: "none",
+    })
+    .status(200)
+    .json(new ApiResponse(200, null, "Logout successful"));
 });
 
 export const forgotPassword = asyncHandler(async (req, res) => {
-    await forgotPasswordService(req.body.email);
+  await forgotPasswordService(req.body.email);
 
-    return res
-        .status(200)
-        .json(new ApiResponse(200, null, "Password reset link sent successfully"));
+  return res
+    .status(200)
+    .json(new ApiResponse(200, null, "Password reset link sent successfully"));
 });
 
-export const getMe= asyncHandler(async (req, res) => {
-    const user = await getMeService(req.user._id);
+export const getMe = asyncHandler(async (req, res) => {
+  const user = await getMeService(req.user._id);
 
-    return res
-        .status(200)
-        .json(new ApiResponse(200, user, "User fetched successfully"));
+  return res
+    .status(200)
+    .json(new ApiResponse(200, user, "User fetched successfully"));
 });
